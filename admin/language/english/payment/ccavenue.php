f8881602383c39d98fe63021fdd42c7f4b21fd39
M R Raja <tezpur4u@gmail.com>
2015-12-08 14:10:24 +0530

    Create ccavenue.php

---
+++ admin/language/english/payment/ccavenue.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']			= 'CCAvenue - M R Raja';
+
+// Text
+$_['text_payment']			= 'Payment';
+$_['text_success']			= 'Success: You have modified CCavenue!';
+$_['text_cavenue']			= '<a onclick="window.open(\'http://www.ccavenue.com/\');"><img src="http://www.ccavenue.com/images/ccavenue_home_logo.gif" style="border: 1px solid #EEEEEE;" /></a>';
+
+// Entry
+$_['entry_merchant']		= 'Merchant Account ID:';
+$_['entry_working_key']			= 'Merchant Working Key:';
+$_['entry_total']             = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active.</span>';
+$_['entry_order_status']      = 'Order Status:';
+$_['entry_geo_zone']          = 'Geo Zone:';
+$_['entry_status']            = 'Status:';
+$_['entry_sort_order']        = 'Sort Order:';
+
+// Error
+$_['error_permission']			= 'Warning: You do not have permission to modify payment Moneybrace!';
+$_['error_merchantacct']		= 'Merchant Account required!';
+$_['error_cert']			= 'Merchant certificates required!';
+?>
+
+<?php
+// Text
+$_['text_title'] = 'Credit Card / Debit Card (CCAvenue)';
+?>
